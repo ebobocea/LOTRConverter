2584d4e869b66ffa94ed40825773517bfcdf106a
Elisei Bobocea <elisey93@icloud.com>
2022-10-18 09:54:51 +0100

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Elisei Bobocea on 18/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
